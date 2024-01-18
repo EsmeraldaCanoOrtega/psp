7cba44bfa860129d088eb49c8d949d61e66cdc98
EsmeraldaCanoOrtega <106031506+EsmeraldaCanoOrtega@users.noreply.github.com>
2024-01-18 19:42:28 +0100

    Delete Thread/EjecHilos.java

--- Thread/EjecHilos.java
+++
@@ -1,9 +0,0 @@
-public class EjecHilos {
-    public static void main(String[] args) {
-        Thread hiloPar = new Thread(new HiloParImparR(2));
-        Thread hiloImpar = new Thread(new HiloParImparR(1));
-        hiloImpar.setPriority(10);
-        hiloPar.start();
-        hiloImpar.start();
-    }
-}
